1f85180f7642f7aa8016e7018733d0afa43c50b2
Hassan <hassan.sobhy@galaxysoft.ws>
2024-06-30 14:25:16 +0300

    change base url

--- lib/data/sources/remote/yemensoft/api_keys.dart
+++ lib/data/sources/remote/yemensoft/api_keys.dart
@@ -1,5 +1,5 @@
 class APIKeys {
-  static const String baseUrl = "http://mapp.yemensoft.net/";
+  static const String baseUrl = "http://mdev.yemensoft.net:8087/";
   static const String login = "OnyxDeliveryService/Service.svc/CheckDeliveryLogin";
   static const String bills = "OnyxDeliveryService/Service.svc/GetDeliveryBillsItems";
   static const String statusTypes = "OnyxDeliveryService/Service.svc/GetDeliveryStatusTypes";
--- lib/data/sources/remote/yemensoft/orders/remote/remote_status_types.g.dart
+++ lib/data/sources/remote/yemensoft/orders/remote/remote_status_types.g.dart
@@ -9,8 +9,7 @@ part of 'remote_status_types.dart';
 RemoteStatusTypes _$RemoteStatusTypesFromJson(Map<String, dynamic> json) =>
     RemoteStatusTypes(
       statusTypes: (json['DeliveryStatusTypes'] as List<dynamic>?)
-              ?.map(
-                  (e) => RemoteStatusType.fromJson(e as Map<String, dynamic>))
+              ?.map((e) => RemoteStatusType.fromJson(e as Map<String, dynamic>))
               .toList() ??
           const [],
     );
